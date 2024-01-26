c5294073450cd7a13efa8e02c00cd8e0bc40db79
zachd9757 <deshawz@wwu.edu>
2024-01-25 17:41:30 -0800

    chip init progress

--- chip-8.c
+++ chip-8.c
@@ -1,18 +1,54 @@
-// A CHIP-8 emulator (interpreter) based on this guie: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 // Author: Zach DeShaw
+// A CHIP-8 emulator (interpreter) based on this guie: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "chip-8.h"
 
-// Initializes the interpreter
+const char* font[] = {
+    "0xF0", "0x90", "0x90", "0x90", "0xF0", // 0
+    "0x20", "0x60", "0x20", "0x20", "0x70", // 1
+    "0xF0", "0x10", "0xF0", "0x80", "0xF0", // 2
+    "0xF0", "0x10", "0xF0", "0x10", "0xF0", // 3
+    "0x90", "0x90", "0xF0", "0x10", "0x10", // 4
+    "0xF0", "0x80", "0xF0", "0x10", "0xF0", // 5
+    "0xF0", "0x80", "0xF0", "0x90", "0xF0", // 6
+    "0xF0", "0x10", "0x20", "0x40", "0x40", // 7
+    "0xF0", "0x90", "0xF0", "0x90", "0xF0", // 8
+    "0xF0", "0x90", "0xF0", "0x10", "0xF0", // 9
+    "0xF0", "0x90", "0xF0", "0x90", "0x90", // A
+    "0xE0", "0x90", "0xE0", "0x90", "0xE0", // B
+    "0xF0", "0x80", "0x80", "0x80", "0xF0", // C
+    "0xE0", "0x90", "0x90", "0x90", "0xE0", // D
+    "0xF0", "0x80", "0xF0", "0x80", "0xF0", // E
+    "0xF0", "0x80", "0xF0", "0x80", "0x80"  // F
+};
+
+/* 
+ * Initializes the interpreter.
+ */
 void init(struct CHIP8* chip) {
     // Init memory
-    chip = (struct CHIP8*) calloc(1, sizeof(struct CHIP8*));
+    chip = (struct CHIP8*) malloc(sizeof(struct CHIP8*));
+    if (chip == NULL) {
+        perror("Could not allocate memory");
+    }
 
-    //TODO: init
+    // Store font in memory
+    size_t num_font_hexes = sizeof(font) / sizeof(font[0]);
+
+    for (size_t i = 0; i < num_font_hexes; i++) {
+        //TODO: Find out why the type of memory[x] is wrong type
+        sscanf(font[i], "%hhx", chip->memory[i + FONT_OFFSET]);
+    }
 }
 
+/* 
+ * Entry point of the emulator.
+ */
 int main() {
     // Initialize CHIP8
     struct CHIP8* chip;
-    init(&chip);
-
+    init(chip);
+    printf("%x\n", chip->memory[0x050]);
 }
--- chip-8.h
+++ chip-8.h
@@ -1,6 +1,10 @@
-struct CHIP8 {
-    char memory[4096];  // RAM, should be writeable
-    int pc;             // Program counter
-};
 
-//TODO: global variable for memory start (address 200)
+/*============== global variables ==============*/
+#define MEM_START ( 0x200 )     // CHIP-8 programs are to be loaded into memory here (originally, the interpreter was stored in the front of memory, creating a need for this offset)
+#define FONT_OFFSET  ( 0x050 )  // Storage location of the font in memory
+
+/*============== structs ==============*/
+struct CHIP8 {
+    unsigned char memory[4096];          // RAM, should be writeable
+    int pc;                     // Program counter
+};
